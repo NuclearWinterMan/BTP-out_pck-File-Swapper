ad99024e6282ed1f569d8fcc1c253e52980f29f8
NuclearWinterMan <nuclearwinterman555@gmail.com>
2024-02-12 14:37:45 -0700

    Added Text areas

--- src/guiElements/HexViewFrame.java
+++ src/guiElements/HexViewFrame.java
@@ -1,11 +1,15 @@
 package guiElements;
-import javax.swing.JInternalFrame;
+import fileHandling.AssetFile;
 
+import javax.swing.JInternalFrame;
+import javax.swing.JTextArea;
 
 @SuppressWarnings("serial")
 public class HexViewFrame extends JInternalFrame {
 	
-	
+	public ReplacerFrame parentFrame;
+	private JTextArea targetFileReadout;
+	private JTextArea replacementFileReadout;
 	
 	public HexViewFrame() {
 		super("Hex View", true, false, true);
